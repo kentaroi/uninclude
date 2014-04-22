7741f54a0c01d8df2f64b31d8a12438b7bbd5b4b
Kentaro Imai <kentaroi@gmail.com>
2014-04-23 04:33:54 +0900

    Make compatible with 2.1.1

--- ext/uninclude/extconf.rb
+++ ext/uninclude/extconf.rb
@@ -4,4 +4,8 @@ RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 $CFLAGS << " #{ENV["CFLAGS"]}"
 
+if RUBY_VERSION >= '2.1.0'
+  $CFLAGS << ' -DRUBY_2_1_x'
+end
+
 create_makefile('uninclude/uninclude')
--- ext/uninclude/uninclude.c
+++ ext/uninclude/uninclude.c
@@ -4,8 +4,16 @@
 #include <ruby/backward/classext.h>
 #endif
 
-#ifndef RCLASS_M_TBL
-#define RCLASS_M_TBL(c) (RCLASS(c)->m_tbl)
+#ifdef RUBY_2_1_x
+#define RMODULE_IDENTIFIER(c) (RCLASS(c)->m_tbl_wrapper)
+#else
+#define RMODULE_IDENTIFIER(c) (RCLASS(c)->m_tbl)
+#endif
+
+#ifdef RUBY_2_1_x
+#define UNINCLUDE_CLEAR_METHOD_CACHE(c) (rb_clear_method_cache_by_class(c))
+#else
+#define UNINCLUDE_CLEAR_METHOD_CACHE(c) (rb_clear_cache_by_class(c))
 #endif
 
 static void uninclude(VALUE klass, VALUE mod) {
@@ -13,9 +21,9 @@ static void uninclude(VALUE klass, VALUE mod) {
 
   VALUE superklass = RCLASS_SUPER(klass);
   for(; superklass; klass = superklass, superklass = RCLASS_SUPER(klass)) {
-    if(RCLASS_M_TBL(superklass) == RCLASS_M_TBL(mod)) {
+    if(RMODULE_IDENTIFIER(superklass) == RMODULE_IDENTIFIER(mod)) {
       RCLASS_SUPER(klass) = RCLASS_SUPER(superklass);
-      rb_clear_cache_by_class(klass);
+      UNINCLUDE_CLEAR_METHOD_CACHE(klass);
       break;
     }
   };
